276a1944ccd6228918df7ef4a7348cab4a262ad4
Bartłomiej Bułat <bartek.bulat@gmail.com>
2015-02-16 19:59:32 +0100

    Optimization

--- insert-example.rb
+++ insert-example.rb
@@ -4,7 +4,6 @@ require "ruby-prof"
 require "ruby-prof-flamegraph"
 require "moped"
 require "bson"
-require "hashie/mash"
 require "json"
 
 session = Moped::Session.new([ "127.0.0.1:27017" ])
@@ -14,15 +13,16 @@ session.drop
 result = RubyProf.profile do
 
   Dir['cities/*.json'].each do |file|
-    geocode = Hashie::Mash.new(JSON.load(File.open(file)))
-    interp = geocode.interpretations.first
+    geocode = JSON.load(File.open(file))
+    interp = geocode['interpretations'].first
 
     if interp
-      name = interp.feature.displayName
-      country_code = interp.feature.cc
-      point = interp.feature.geometry.center
+      feature = interp['feature']
+      name = feature['displayName']
+      country_code = feature['cc']
+      point = feature['geometry']['center']
 
-      session[:capitals].insert(:name => name, :country_code => country_code, :coordinates => [point.lat, point.lng ])
+      session[:capitals].insert(:name => name, :country_code => country_code, :coordinates => [point['lat'], point['lng'] ])
     end
   end
 
